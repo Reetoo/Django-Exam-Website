b09cf1459c9a68ca5b9580832d3bea681c04fa94
evelene law <lawruixi@gmail.com>
2022-02-22 12:53:07 +0000

    added a bunch of files for ibmcloud pushing

--- myproject/settings.py
+++ myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'aay0j_9b&ky3a7(8m8il+-1ud(scw12@w5!+5-=gsk6ynzi0ls'
 DEBUG = True
 
 # <HINT> add your cloud host here
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["p8080-5c8e2208-323b-463b-92db-b725f5889587.dp3.jp-tok.shell.cloud.ibm.com", "lawruixi_3v3.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
